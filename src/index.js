1f9157a5646fffa5a1ff9f29f5cd6219b2c005a4
IreniaYulieth-94 <ireniayulieth.sierrasalazar@gmail.com>
2022-11-18 20:43:38 -0500

    firts commit instaya project

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import App2 from './App2';
+import reportWebVitals from './reportWebVitals';
+
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<App/>} />
+        <Route path="/index" element={<App2/>} />
+      </Routes>
+    </Router>
+</React.StrictMode>
+
+,
+
+document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
